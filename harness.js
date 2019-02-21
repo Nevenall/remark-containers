891cc81a8520191a083010a87d758cbe88684c8c
Dan Behlings <nevenall@hotmail.com>
2019-02-21 13:18:17 -0800

    pretty good version of custom containers

--- harness.js
+++ harness.js
@@ -9,7 +9,21 @@ var containers = require('./src/index')
 
 var processor = unified()
    .use(markdown)
-   .use(containers)
+   .use(containers, {
+      type: 'figure-table',
+      element: 'figure',
+      transform: (node, config, tokenize) => {
+       
+         node.children.push({
+            type: 'figcaption',
+            data: {
+               hName: 'figcaption'
+            },
+            children: tokenize(config)
+         })
+
+      }
+   })
    .use(remark2rehype)
    .use(html)
 
@@ -20,13 +34,14 @@ processor.process(`
 
 Some text and stuff. 
 
-::: figure figure-table
+::: figure-table Table 01 — A Simple Sample Table
 one | two | three
 ---|---|---
 a | b | c
-::: figcaption
-Tested nested containers
 :::
+
+::: container
+this should render as ordinary text.
 :::
 
 and there is more stuff after.
--- src/index.js
+++ src/index.js
@@ -1,62 +1,33 @@
-// what if we could use rehype to parse some option html so we have the data
-// to pass to the add node
-// yes, we can easily parse a fragment into a hast and then maybe merge that into the 
-// mast tree so
-
-// trigger on ::: type option option some phrase that is the rest of container specification
-
-
-
-
-// confguration is an important part of containers
-// you can configure the plugin multiple times 
-// once per type
-// the trick is configuring options. 
-// we want to be able to pass in various class names. left and right for example, 
-// but also phrases, like the caption for tables. 
-// we could do it with a reg exp? type, options 
-
-// but I think we need better configs
-// type has to be a word
-// than configure possible options as an array?
-// actually, what if we could specify a template?
-
-// like when we define the container
-// we have <div class='{option1}'><p>{remainder}</p> </div> 
-// than hopefully we can precompile a tree we can inject the values into. 
-
-// the container options specifies a type, then an array of option objects
-// each option is one word, except the option will be the remainder of the line. 
-// options are {name: option1, }
-
-
-// might be better to do terms like that too. 
-// just need open and closing markers
-
 var regex = /:::\s*(?<type>\S+)[\r\t\f\v ]*(?<options>.*?)\n(?<body>.*):::/s
-
-
 
 function plugin(options) {
 
    options = options || {}
 
-   function tokenizer(eat, value, silent) {
+   // if options is valid, with a type, and a transform function
+   // then we should create a special function named typeContainer and add that
+   // so, if we don't specify a custom container, would we to still add the default?
+   // what would be the most expected? I think not do it.
+
+   var useCustomTokenizer = false
+
+   if (options.type !== undefined && options.type !== '' && options.transform !== undefined) {
+      useCustomTokenizer = true
+   }
+
+   function defaultTokenizer(eat, value, silent) {
       if (value.startsWith(":::")) {
          // might be a match
          var m = regex.exec(value)
 
          if (m) {
+
             if (silent) return true
 
             var [type, config, body] = [m.groups.type, m.groups.options, m.groups.body]
 
-            // or we might have to structure it so you can inject a function which can translate the parts of the container into whatever you want. 
-            var endOfLine = value.indexOf('\n')
-            var header = value.substring(0, endOfLine)
-
-            eat(header)
-            eat('\n')
+            // eat the header line
+            eat(value.substring(0, value.indexOf('\n') + 1))
 
             var exit = this.enterBlock()
 
@@ -65,11 +36,11 @@ function plugin(options) {
             var node = {
                type: type,
                data: {
-                  hName: options.element || type
+                  hName: type
                }
             }
 
-            // if there is a config string, use that as the class
+            // if there is a config string, use that as the element class
             if (config.trim() !== '') {
                node.data.hProperties = {
                   className: config
@@ -77,7 +48,7 @@ function plugin(options) {
             }
 
             node.children = this.tokenizeBlock(body, eat.now())
-            
+
             add(node)
 
             exit()
@@ -86,13 +57,56 @@ function plugin(options) {
       }
    }
 
-   const Parser = this.Parser
+   function customTokenizer(eat, value, silent) {
+      if (value.startsWith(":::")) {
+         // might be a match
+         var m = regex.exec(value)
 
+         // only match containers of the specified type
+         if (m && m.groups.type === options.type) {
+
+            if (silent) return true
+
+            var [type, config, body] = [m.groups.type, m.groups.options, m.groups.body]
+            
+            var exit = this.enterBlock()
+            var now = eat.now()
+            
+            // eat the header line
+            eat(value.substring(0, value.indexOf('\n') + 1))
+
+            var add = eat(body)
+
+            var node = {
+               type: type,
+               data: {
+                  hName: options.element || 'div'
+               }
+            }
+
+            node.children = this.tokenizeBlock(body, now)
+            // pass the transform a tokenize function with the current location in case they want to parse the config 
+            options.transform(node, config, (value) => this.tokenizeInline(value, now))
+
+            add(node)
+
+            eat(':::')
+            exit()
+         }
+      }
+   }
+
+   const Parser = this.Parser
    const blockTokenizers = Parser.prototype.blockTokenizers
    const blockMethods = Parser.prototype.blockMethods
-   blockTokenizers.customContainers = tokenizer
-   blockMethods.splice(blockMethods.indexOf('fencedCode') + 1, 0, 'customContainers')
 
+   if (useCustomTokenizer) {
+      blockTokenizers[options.type] = customTokenizer
+      blockMethods.splice(blockMethods.indexOf('fencedCode') + 1, 0, options.type)
+   } else {
+      blockTokenizers.containers = defaultTokenizer
+      blockMethods.splice(blockMethods.indexOf('fencedCode') + 1, 0, 'containers')
+   }
 }
 
 
